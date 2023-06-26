21bdc0bf1f6e48fc1e23ef9e8f1511933d57170b
jack <jackg2667@protonmail.com>
2023-06-26 16:50:23 +0100

    starting the game

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,20 @@
-console.log("hello world")
-console.log("hello world")
-console.log("hello world")
-console.log("hello world")
-console.log("hello world")
-console.log("hello world")
-console.log("hello world")
-console.log("hello world")
-console.log("hello world")
-console.log("hello world")
-console.log("hello world")
-console.log("hello world")
-console.log("hello world")
-console.log("hello world")
-console.log("hello world")
-console.log("hello world")
-console.log("hello world")
-console.log("hello world")
+let firstCard = 10;
+let secondCard = 11;
+let won = false;
+let sum = firstCard + secondCard;
+
+
+if (sum <= 20) {
+    console.log("draw another card")
+}
+
+else if (sum == 21) {
+    console.log("win")
+    won = true;
+}
+
+else { 
+    console.log("The sum is less than 21:")
+}
+
+console.log(won);
